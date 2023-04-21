338b93855467857b14d5e58eafcc5c2f7f832d92
samsonalxgit <samsonmanie2022@gmail.com>
2023-04-21 13:29:15 -0700

    Puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
-#start with puppet
+#resource declaration start with puppet
 file { '/tmp/codingschool:'
-ensure => file',
-path => '/tmp/codingschool',
+ensure => file,
+path => '/tmp/icodingschool',
 mode => '0744',
 owner => 'www-data',
 group => 'www-data',
