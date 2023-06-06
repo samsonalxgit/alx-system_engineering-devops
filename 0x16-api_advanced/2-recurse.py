67461dd86e3634baf6916d44572834e60f26eecc
samsonalxgit <samsonmanie2022@gmail.com>
2023-06-06 13:55:48 -0700

    requesting the reddit api

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,28 +1,39 @@
 #!/usr/bin/python3
-'''a recursive function that queries the Reddit API and returns a list containing the titles of all hot articles for a given subreddit.'''
+"""script for parsing web data from an api
+"""
+import json
 import requests
-
-
-after = None
+import sys
 
 
 def recurse(subreddit, hot_list=[]):
-    """returning top ten post titles recursively"""
-    global after
-    user_agent = {'User-Agent': 'api_advanced-project'}
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    parameters = {'after': after}
-    results = requests.get(url, params=parameters, headers=user_agent,
-                           allow_redirects=False)
-
-    if results.status_code == 200:
-        after_data = results.json().get("data").get("after")
-        if after_data is not None:
-            after = after_data
-            recurse(subreddit, hot_list)
-        all_titles = results.json().get("data").get("children")
-        for title_ in all_titles:
-            hot_list.append(title_.get("data").get("title"))
-        return hot_list
+    """api call to reddit to get the number of subscribers
+    """
+    base_url = 'https://www.reddit.com/r/{}/top.json'.format(
+        subreddit
+    )
+    headers = {
+        'User-Agent':
+        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
+        Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
+    }
+    if len(hot_list) == 0:
+        # grab info about all users
+        url = base_url
     else:
-        return (None)
+        url = base_url + '?after={}_{}'.format(
+            hot_list[-1].get('kind'),
+            hot_list[-1].get('data').get('id')
+        )
+    response = requests.get(url, headers=headers)
+    resp = json.loads(response.text)
+    try:
+        # grab the info about the users' tasks
+        data = resp.get('data')
+        children = data.get('children')
+    except:
+        return None
+    if children is None or data is None or len(children) < 1:
+        return hot_list
+    hot_list.extend(children)
+    return recurse(subreddit, hot_list)
